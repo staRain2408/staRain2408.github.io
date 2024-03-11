08015b5c412077e3f09b39c8ff4cac1d4da22fcd
staRain2408 <68314067+staRain2408@users.noreply.github.com>
2024-03-11 15:43:59 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,3 +2,76 @@
 function closeOverlay() {
     document.querySelector('.overlay').style.display = 'none';
 }
+
+var temp = '';
+
+function mOver(obj){
+    obj.style.backgroundColor="rgba(85, 85, 85, 0.7)";
+    obj.style.color="rgba(255, 255, 255)";
+    temp = obj.innerHTML;
+    return temp;
+
+}
+function mOut(obj){
+    mOver(obj)
+    obj.style.backgroundColor="rgba(185, 185, 185, 0)";
+    obj.style.color="rgba(0, 0, 0)";
+    obj.innerHTML=temp; 
+    
+}
+
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function mDown_A(obj){
+    document.getElementById('A').hidden = false;
+    document.getElementById('B').hidden = true;
+    document.getElementById('C').hidden = true;
+    document.getElementById('D').hidden = true;
+    document.getElementById('E').hidden = true;
+    
+}
+
+function mDown_B(obj){
+    document.getElementById('A').hidden = true;
+    document.getElementById('B').hidden = false;
+    document.getElementById('C').hidden = true;
+    document.getElementById('D').hidden = true;
+    document.getElementById('E').hidden = true;
+    
+}
+
+function mDown_C(obj){
+    document.getElementById('A').hidden = true;
+    document.getElementById('B').hidden = true;
+    document.getElementById('C').hidden = false;
+    document.getElementById('D').hidden = true;
+    document.getElementById('E').hidden = true;
+    
+}
+
+function mDown_D(obj){
+    document.getElementById('A').hidden = true;
+    document.getElementById('B').hidden = true;
+    document.getElementById('C').hidden = true;
+    document.getElementById('D').hidden = false;
+    document.getElementById('E').hidden = true;
+    
+}
+
+function mDown_E(obj){
+    document.getElementById('A').hidden = true;
+    document.getElementById('B').hidden = true;
+    document.getElementById('C').hidden = true;
+    document.getElementById('D').hidden = true;
+    document.getElementById('E').hidden = false;
+    
+}
+
+function mUp(obj){
+    document.getElementById('top').hidden = false;
+}
