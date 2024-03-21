601b2c27fd2d8ccc413889be98a2792b52efd5df
staRain2408 <68314067+staRain2408@users.noreply.github.com>
2024-03-22 00:48:14 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,14 +1,8 @@
-// JavaScript函数以关闭覆盖层
-function closeOverlay() {
-    document.querySelector('.overlay').style.display = 'none';
-}
-
 var temp = '';
-
 function mOver(obj){
     obj.style.backgroundColor="rgba(139, 58, 26, 0.3)";
-    obj.style.color="rgba(255, 255, 255)";
-    obj.style.fontWeigth = 100;
+    obj.style.color="rgba(245, 255, 255)";
+    obj.style.borderBottom="5px solid rgba(105, 46, 21, 0.550)";
     temp = obj.innerHTML;
     return temp;
 
@@ -17,6 +11,7 @@ function mOut(obj){
     temp = mOver(obj)
     obj.style.backgroundColor="rgba(228, 131, 52, 0)";
     obj.style.color="rgb(124, 54, 26)";
+    obj.style.borderBottom="";
     obj.innerHTML=temp;   
 }
 
@@ -74,4 +69,17 @@ function mDown_E(obj){
 
 function mUp(obj){
     document.getElementById('top').hidden = false;
+}
+
+
+
+
+// 姓名中英變換
+function mOvername(obj){
+    obj.innerHTML = "楊翔瑜的首頁";
+    obj.style.fontWeight="bold";
+}
+function mOutname(obj){
+    obj.innerHTML = "Hsiang-Yu's Homepage";
+    obj.style.fontWeight="";
 }
